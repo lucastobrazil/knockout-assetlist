395ea0c572fbff106886d40dad08ad8aed2352fd
lucas@arundelsounds.com <lucas@arundelsounds.com>
2015-05-25 23:33:12 +1000

    Added sidebar

--- src/app.js
+++ src/app.js
@@ -1,15 +1,37 @@
 var ViewModel = function () {
     var self = this;
     self.files = ko.observableArray([ ]);
+	self.sideBarVisible = ko.observable(false);
+    self.selectedRow = ko.observable();
     
     $.getJSON( "data/data.json", function(data) {
-			self.files(data.files)
-		});
+        self.files(data.files);
+        self.selectedRow(self.files()[0]);
+    });
 
-	// Extra little function to show that it's a ko array
+    // Delete the item from the array
     self.removeFile = function() {
         self.files.remove(this);
+        self.selectedRow()[0];
     };
+
+    // View the sidebar 
+    self.toggleSideBar = function(){
+        self.sideBarVisible(!self.sideBarVisible());
+    };
+
+    // User selected a row
+    self.didSelectRow = function(){
+        if(self.selectedRow().filename === this.filename){
+            self.sideBarVisible(false);
+            self.selectedRow(false);
+        }else{
+            self.sideBarVisible(true);
+            self.selectedRow(this);
+
+        }
+    }
+    
 };
 
 $(document).ready(function() {
